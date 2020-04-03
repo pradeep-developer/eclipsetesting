1f4ea6f64af3bb81999119f6fea68b4ff388cd1f
pradeepsmdl <pradeepsmdl@pradeepsmdlsmbp.lan1>
2020-04-03 09:20:07 -0700

    apache camel-1 project push

---
+++ ApacheCamel-1/src/test/java/com/example/self/ApacheCamel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.self;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamel1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
